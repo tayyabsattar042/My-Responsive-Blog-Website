5b830314cc48b79aae5d9fe71e9e67139e9d6a19
tayyabsattar042 <tayyabsattar38@gmail.com>
2023-09-11 12:18:12 +0500

    My Responsive Blog Website..

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 const nav = document.querySelector('.mobile-nav');
 const navMenuBtn = document.querySelector('.nav-menu-btn');
 const navCloseBtn = document.querySelector('.nav-close-btn');
-const navToggleFunc = function () { nav.classList.toggle('active'); }
+const navToggleFunc = function ()
+  {
+   nav.classList.toggle('active'); 
+  }
 navMenuBtn.addEventListener('click', navToggleFunc);
 navCloseBtn.addEventListener('click', navToggleFunc);
 
